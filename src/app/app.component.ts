52ff2c6e6a77c2ebac199fc13cc18b26adb94a05
ShaliaRamiah <shalia.r20@gmail.com>
2023-05-05 22:52:22 +0200

    retrieve temperature (undefined)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,49 +2,47 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
+
+export class AppComponent implements OnInit {
   title = 'angular-weather-app';
 
-  //declaring a string property 
   coordinates!: string;
+  weatherData: any = {};
 
-  //getting user's location
-  public getLocation(): void {
-    //checking if the browser supports geolocation
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation(): void {
     if (navigator.geolocation) {
-      //if it is supported, get users position
       navigator.geolocation.getCurrentPosition(position => {
         this.showPosition(position);
+        console.log(position);
       });
-    } else { 
-      //if not, display error
-      this.coordinates = "Geolocation is not supported by this browser.";
+    } else {
+      this.coordinates = 'Geolocation is not supported by this browser.';
     }
   }
 
-  //show users current position
   showPosition(position: GeolocationPosition): void {
-    //setting coordinates to users long and lat 
     this.coordinates = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
-    this.getData(position.coords.latitude, position.coords.longitude).subscribe(res => {
-      console.log(res);
-    })
+    this.getTemperature(position.coords.latitude, position.coords.longitude).subscribe(temp => {
+      this.weatherData.temperature = temp;
+      console.log(this.weatherData); 
+    });
   }
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-  }
-
-  public getData(latitude: number, longitude: number): Observable<any[]> {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
-    return this.http.get<any[]>(url)
-      .pipe(map(res => res))
+  getTemperature(latitude: number, longitude: number): Observable<any> {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+    return this.http.get<any>(url).pipe(map(res => res.hourly[0]?.temperature_2m));
   }
 }
