7364bdda893896e30386b9c74e9b86467e8a0825
ShaliaRamiah <shalia.r20@gmail.com>
2023-05-05 17:53:31 +0200

    joining functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,42 @@ import { Observable, map } from 'rxjs';
 })
 
 export class AppComponent implements OnInit{
+  title = 'angular-weather-app';
+
+  //declaring a string property 
   coordinates!: string;
 
+  //getting user's location
   public getLocation(): void {
-
+    //checking if the browser supports geolocation
     if (navigator.geolocation) {
+      //if it is supported, get users position
       navigator.geolocation.getCurrentPosition(position => {
         this.showPosition(position);
       });
     } else { 
+      //if not, display error
       this.coordinates = "Geolocation is not supported by this browser.";
     }
   }
 
+  //show users current position
   showPosition(position: GeolocationPosition): void {
+    //setting coordinates to users long and lat 
     this.coordinates = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
-  }
-
-
-  
-  constructor (private http:HttpClient) {}
-  title = 'angular-weather-app';
-  ngOnInit(): void {
-    this.getData().subscribe(res => {
+    this.getData(position.coords.latitude, position.coords.longitude).subscribe(res => {
       console.log(res);
     })
   }
 
-  public getData(): Observable<any[]> {
-    const longitude = 0;
-    const latitude = 0;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  public getData(latitude: number, longitude: number): Observable<any[]> {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
     return this.http.get<any[]>(url)
       .pipe(map(res => res))
   }
-
 }
-
